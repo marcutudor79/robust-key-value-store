f360f37027e87c26d44a2a44e42a2cc54a828d77
daniele.fama <daniele.fama@telecom-paris.fr>
2025-12-24 13:17:57 +0100

    add main

--- key-value-store/src/main/java/code/App.java
+++ key-value-store/src/main/java/code/App.java
@@ -1,7 +1,44 @@
 package code;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
+	public static void main(String[] args) {
+		final int N = 3; 
+		final int f = 1; 
+
+		ActorSystem system = ActorSystem.create("system");
+		List<ActorRef> processes = new ArrayList<>();
+
+		// create processes
+		for (int i = 0; i < N; i++) 
+			processes.add(system.actorOf(Process.createActor(), "p" + i));
+				
+		// send ref
+		ReferencesMessage refs = new ReferencesMessage(processes);
+		for (ActorRef p : processes) 
+			p.tell(refs, ActorRef.noSender());
+		
+
+		// crash
+		List<ActorRef> toCrash = new ArrayList<>(processes);
+		Collections.shuffle(toCrash);
+		
+		for (int i = 0; i < f; i++) 
+			toCrash.get(i).tell(new CrashMessage(), ActorRef.noSender());
+		
+		// launch
+		LaunchMessage launch = new LaunchMessage();
+		// try {			
+		// 	Thread.sleep(1000); 
+		// } catch (InterruptedException e) { e.printStackTrace(); }
+		
+		for (int i = f; i < N; i++) 
+			toCrash.get(i).tell(launch, ActorRef.noSender());
+		
+	}
 }
